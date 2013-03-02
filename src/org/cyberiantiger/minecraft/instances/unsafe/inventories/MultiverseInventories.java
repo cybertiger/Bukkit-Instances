2e5c06dabde7cf04c323b07f84f449d55a196ec2
Antony Riley <antony@cyberiantiger.org>
2013-03-02 18:36:27 +0200

    More twat checks when calling into Multiverse-Inventories.

--- src/org/cyberiantiger/minecraft/instances/unsafe/inventories/MultiverseInventories.java
+++ src/org/cyberiantiger/minecraft/instances/unsafe/inventories/MultiverseInventories.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.cyberiantiger.minecraft.instances.unsafe.inventories;
 
 import com.onarandombox.multiverseinventories.api.profile.WorldGroupProfile;
@@ -12,6 +11,7 @@ import com.onarandombox.multiverseinventories.api.profile.WorldGroupProfile;
  * @author antony
  */
 public class MultiverseInventories implements Inventories {
+
     private final com.onarandombox.multiverseinventories.api.Inventories inventories;
 
     public MultiverseInventories(com.onarandombox.multiverseinventories.api.Inventories inventories) {
@@ -19,15 +19,18 @@ public class MultiverseInventories implements Inventories {
     }
 
     public void addShare(String source, String destination) {
-        for (WorldGroupProfile profile : inventories.getGroupManager().getGroupsForWorld(source)) {
-            profile.addWorld(destination);
+        if (inventories.isEnabled()) {
+            for (WorldGroupProfile profile : inventories.getGroupManager().getGroupsForWorld(source)) {
+                profile.addWorld(destination);
+            }
         }
     }
 
     public void removeShare(String source, String destination) {
-        for (WorldGroupProfile profile : inventories.getGroupManager().getGroupsForWorld(source)) {
-            profile.removeWorld(destination);
+        if (inventories.isEnabled()) {
+            for (WorldGroupProfile profile : inventories.getGroupManager().getGroupsForWorld(source)) {
+                profile.removeWorld(destination);
+            }
         }
     }
-
 }
