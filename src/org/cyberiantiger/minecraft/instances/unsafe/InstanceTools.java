d7e864bab0f6706c3e810de33597816c7cf786f4
Antony Riley <antony@cyberiantiger.org>
2013-03-12 06:59:39 +0200

    Do not keep instanced worlds in memory, do not break craftbukkit and mcmmo [#4]

--- src/org/cyberiantiger/minecraft/instances/unsafe/InstanceTools.java
+++ src/org/cyberiantiger/minecraft/instances/unsafe/InstanceTools.java
@@ -4,36 +4,30 @@
  */
 package org.cyberiantiger.minecraft.instances.unsafe;
 
+import com.google.common.io.Files;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.minecraft.server.v1_4_R1.Chunk;
-import net.minecraft.server.v1_4_R1.ChunkCoordIntPair;
 import net.minecraft.server.v1_4_R1.ChunkRegionLoader;
-import net.minecraft.server.v1_4_R1.Entity;
-import net.minecraft.server.v1_4_R1.EntityHuman;
 import net.minecraft.server.v1_4_R1.EntityTracker;
-import net.minecraft.server.v1_4_R1.ExceptionWorldConflict;
+import net.minecraft.server.v1_4_R1.IAsyncChunkSaver;
 import net.minecraft.server.v1_4_R1.IChunkLoader;
 import net.minecraft.server.v1_4_R1.IDataManager;
 import net.minecraft.server.v1_4_R1.IWorldAccess;
 import net.minecraft.server.v1_4_R1.MethodProfiler;
 import net.minecraft.server.v1_4_R1.MinecraftServer;
 import net.minecraft.server.v1_4_R1.NBTTagCompound;
-import net.minecraft.server.v1_4_R1.PlayerFileData;
-import net.minecraft.server.v1_4_R1.TileEntity;
 import net.minecraft.server.v1_4_R1.WorldData;
 import net.minecraft.server.v1_4_R1.WorldManager;
 import net.minecraft.server.v1_4_R1.WorldProvider;
 import net.minecraft.server.v1_4_R1.WorldServer;
 import net.minecraft.server.v1_4_R1.NBTCompressedStreamTools;
+import net.minecraft.server.v1_4_R1.ServerNBTManager;
 import net.minecraft.server.v1_4_R1.WorldProviderHell;
 import net.minecraft.server.v1_4_R1.WorldProviderTheEnd;
 import org.bukkit.World;
@@ -43,6 +37,7 @@ import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.generator.ChunkGenerator;
 import org.cyberiantiger.minecraft.instances.Coord;
+import org.cyberiantiger.minecraft.instances.FileUtils;
 import org.cyberiantiger.minecraft.instances.Instances;
 import org.cyberiantiger.minecraft.instances.PortalPair;
 import org.cyberiantiger.minecraft.instances.generator.VoidGenerator;
@@ -52,8 +47,9 @@ import org.cyberiantiger.minecraft.instances.generator.VoidGenerator;
  * @author antony
  */
 public final class InstanceTools {
+    public static final String FOLDER_NAME = "worlds";
 
-    public static org.bukkit.World createInstance(Instances instances, PortalPair portal, String sourceWorld) {
+    public static org.bukkit.World createInstance(final Instances instances, PortalPair portal, String sourceWorld) {
         File dataFolder = new File(instances.getServer().getWorldContainer(), sourceWorld);
         if (!dataFolder.isDirectory()) {
             instances.getLogger().info("Failed to create instance, could not find data folder " + dataFolder.getAbsolutePath() + " for world " + sourceWorld);
@@ -66,9 +62,6 @@ public final class InstanceTools {
             return null;
         }
 
-        // CraftWorld bukkitWorld = (CraftWorld) sourceWorld;
-        // WorldServer mcWorld = bukkitWorld.getHandle();
-
         int i = 0;
         while (instances.getServer().getWorld(sourceWorld + '-' + i) != null) {
             i++;
@@ -76,8 +69,44 @@ public final class InstanceTools {
 
         String instanceName = sourceWorld + '-' + i;
 
+        File worldFolder = new File(instances.getDataFolder(), FOLDER_NAME);
+        File saveDataFolder = new File(worldFolder, instanceName);
+
+        if (saveDataFolder.isDirectory()) {
+            try {
+                final File tempFile = File.createTempFile("deleted_",".world", worldFolder);
+                tempFile.delete();
+                if(saveDataFolder.renameTo(tempFile)) {
+                    instances.getLogger().info("Renamed old world data");
+                    instances.getServer().getScheduler().runTaskAsynchronously(instances, new Runnable() {
+
+                        public void run() {
+                            try {
+                                if (!FileUtils.deleteRecursively(tempFile)) {
+                                    instances.getLogger().warning("Failed to delete archived instances world: " + tempFile);
+                                }
+                            } catch (IOException e) {
+                                instances.getLogger().warning("Failed to delete archived instances world: " + tempFile);
+                            }
+                        }
+                        
+                    });
+                } else {
+                    if (!FileUtils.deleteRecursively(saveDataFolder)) {
+                        instances.getLogger().log(Level.SEVERE, "Failed to delete world folder: " + saveDataFolder);
+                        return null;
+                    }
+                }
+            } catch (IOException ex) {
+                instances.getLogger().log(Level.SEVERE, null, ex);
+                return null;
+            }
+        }
+
+        saveDataFolder.mkdirs();
+
         IDataManager dataManager =
-                new InstanceDataManager(instances, dataFolder, instanceName);
+                new InstanceDataManager(instances, dataFolder, saveDataFolder);
 
         // XXX: Copy paste from craftbukkit.
         int dimension = 10 + console.worlds.size();
@@ -130,168 +159,143 @@ public final class InstanceTools {
         return instanceWorld.getWorld();
     }
 
-    private static class InstanceDataManager implements IDataManager, PlayerFileData {
+    // Have to extend WorldNBTStorage - CB casts IDataManager to it in World.getWorldFolder()
+    // cannot just implement IDataManager, PlayerFileData
+    private static class InstanceDataManager extends ServerNBTManager {
+        private static final String WORLD_DATA = "level.dat";
+        private static final String WORLD_DATA_OLD = "level.dat_old";
 
         private final Instances instances;
-        private final File dataFolder;
+        private final File loadDataFolder;
         private final String world;
         private final UUID uid;
         private WorldData worldData;
 
-        public InstanceDataManager(Instances instances, File dataFolder, String world) {
+        public InstanceDataManager(Instances instances, File loadDataFolder, File saveDataFolder) {
+            // false flag - do not create players directory.
+            super(saveDataFolder.getParentFile(), saveDataFolder.getName(), false);
             this.instances = instances;
-            this.dataFolder = dataFolder;
-            this.world = world;
+            this.loadDataFolder = loadDataFolder;
+            this.world = saveDataFolder.getName();
             this.uid = UUID.randomUUID();
-
-            File level = new File(dataFolder, "level.dat");
-
-            WorldData worldData = null;
-
-            if (level.exists()) {
-                try {
-                    NBTTagCompound tag = NBTCompressedStreamTools.a(new FileInputStream(level));
-                    worldData = new WorldData(tag.getCompound("Data"));
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(InstanceTools.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-
-            if (worldData == null) {
-                level = new File(dataFolder, "level.dat_old");
-                if (level.exists()) {
-                    try {
-                        NBTTagCompound tag = NBTCompressedStreamTools.a(new FileInputStream(level));
-                        worldData = new WorldData(tag.getCompound("Data"));
-                    } catch (FileNotFoundException ex) {
-                        Logger.getLogger(InstanceTools.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            }
-
-            this.worldData = worldData;
         }
 
+        @Override
         public WorldData getWorldData() {
-            return worldData;
-        }
-
-        public void checkSession() throws ExceptionWorldConflict {
-            // NOOP, this is an in memory world.
-        }
-
-        public IChunkLoader createChunkLoader(WorldProvider wp) {
-            File chunkDir;
-            if (wp instanceof WorldProviderHell) {
-                chunkDir = new File(dataFolder, "DIM-1");
-            } else if (wp instanceof WorldProviderTheEnd) {
-                chunkDir = new File(dataFolder, "DIM1");
-            } else {
-                chunkDir = dataFolder;
+            File levelData = new File(getDirectory(), WORLD_DATA);
+            if (levelData.isFile()) {
+                return super.getWorldData();
             }
-            ChunkRegionLoader sourceLoader = new ChunkRegionLoader(chunkDir);
-            return new InstanceChunkLoader(sourceLoader);
+            levelData = new File(getDirectory(), WORLD_DATA_OLD);
+            if (levelData.isFile()) {
+                return super.getWorldData();
+            }
+            
+            File file1 = new File(loadDataFolder, WORLD_DATA);
+            NBTTagCompound nbttagcompound;
+            NBTTagCompound nbttagcompound1;
+            
+            if (file1.exists()) {
+                try {
+                    nbttagcompound = NBTCompressedStreamTools.a((InputStream) (new FileInputStream(file1)));
+                    nbttagcompound1 = nbttagcompound.getCompound("Data");
+                    return new WorldData(nbttagcompound1);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+            
+            file1 = new File(loadDataFolder, WORLD_DATA_OLD);
+            if (file1.exists()) {
+                try {
+                    nbttagcompound = NBTCompressedStreamTools.a((InputStream) (new FileInputStream(file1)));
+                    nbttagcompound1 = nbttagcompound.getCompound("Data");
+                    return new WorldData(nbttagcompound1);
+                } catch (Exception exception1) {
+                    exception1.printStackTrace();
+                }
+            }
+            
+            return null;
         }
 
-        public void saveWorldData(WorldData wd, NBTTagCompound nbttc) {
-            this.worldData = wd;
+        @Override
+        public IChunkLoader createChunkLoader(WorldProvider wp) {
+            File loadChunkDir;
+            File saveChunkDir;
+            if (wp instanceof WorldProviderHell) {
+                loadChunkDir = new File(loadDataFolder, "DIM-1");
+                saveChunkDir = new File(getDirectory(), "DIM-1");
+            } else if (wp instanceof WorldProviderTheEnd) {
+                loadChunkDir = new File(loadDataFolder, "DIM1");
+                saveChunkDir = new File(getDirectory(), "DIM1");
+            } else {
+                loadChunkDir = loadDataFolder;
+                saveChunkDir = getDirectory();
+            }
+            ChunkRegionLoader loadLoader = new ChunkRegionLoader(loadChunkDir);
+            ChunkRegionLoader saveLoader = new ChunkRegionLoader(saveChunkDir);
+            return new InstanceChunkLoader(loadLoader, saveLoader);
         }
 
-        public void saveWorldData(WorldData wd) {
-            this.worldData = wd;
-        }
-
-        public PlayerFileData getPlayerFileData() {
-            return this;
-        }
-
-        public void a() {
-            // This looks like it's supposed to sync to disc.
-            // Noop
-        }
-
+        @Override
         public File getDataFile(String string) {
-            instances.getLogger().warning("Data file: " + string + " requested for instanced world " + world);
-            // new Exception().printStackTrace();
-            return new File(this.dataFolder, string + ".dat");
-        }
-
-        public String g() {
-            // Worldname.
-            return world;
-        }
-
-        public UUID getUUID() {
-            return uid;
-        }
-
-        public void save(EntityHuman eh) {
-            instances.getLogger().warning("Warning: was asked to save player " + eh.getName() + " for instanced world " + world);
-        }
-
-        public void load(EntityHuman eh) {
-            instances.getLogger().warning("Warning: was asked to load player " + eh.getName() + " for instanced world " + world);
-        }
-
-        public String[] getSeenPlayers() {
-            return new String[0];
+            File result = new File(this.loadDataFolder, string + ".dat");
+            if (result.isFile()) {
+                return result;
+            }
+            File source = new File(getDirectory(), string + ".dat");
+            if (!source.isFile()) {
+                return result;
+            }
+            try {
+                Files.copy(source, result);
+            } catch (IOException ex) {
+                instances.getLogger().log(Level.SEVERE, "Error copying " + source.getPath() + " to " + result.getPath() + " for Instance world: " + world, ex);
+            }
+            return result;
         }
     }
 
-    public static final class InstanceChunkLoader implements IChunkLoader {
+    // Safe not to extend ChunkRegionLoader - CB does not cast to ChunkRegionLoader anywhere.
+    public static final class InstanceChunkLoader implements IChunkLoader, IAsyncChunkSaver {
 
-        private final IChunkLoader source;
-        private final Map<ChunkCoordIntPair, Chunk> chunkCache = new HashMap<ChunkCoordIntPair, Chunk>();
+        private final ChunkRegionLoader loadLoader;
+        private final ChunkRegionLoader saveLoader;
 
-        public InstanceChunkLoader(IChunkLoader source) {
-            this.source = source;
+        public InstanceChunkLoader(ChunkRegionLoader loadLoader, ChunkRegionLoader saveLoader) {
+            this.loadLoader = loadLoader;
+            this.saveLoader = saveLoader;
         }
 
         public Chunk a(net.minecraft.server.v1_4_R1.World world, int i, int j) {
-            Chunk cacheChunk = chunkCache.get(new ChunkCoordIntPair(i, j));
-            if (cacheChunk == null) {
-                cacheChunk = source.a(world, i, j);
-            } else {
-                initChunk(cacheChunk);
+            if (saveLoader.chunkExists(world, i, j)) {
+                return saveLoader.a(world, i, j);
             }
-            return cacheChunk;
+            return loadLoader.a(world, i, j);
         }
 
         public void a(net.minecraft.server.v1_4_R1.World world, Chunk chunk) {
-            chunkCache.put(chunk.l(), chunk);
+            saveLoader.a(world, chunk);
         }
 
         public void b(net.minecraft.server.v1_4_R1.World world, Chunk chunk) {
-            // NOOP
+            // Assume this is supposed to be some sort of save operation.
+            // Can't tell from NMS - empty method.
+            saveLoader.b(world, chunk);
         }
 
         public void a() {
-            // Probably a NOOP
-            //System.err.println("InstanceChunkLoader.a()");
-            //Logger.getLogger(CreateInstance.class.getName()).log(Level.SEVERE, null, new Exception());
+            // XXX: Can't guess if this is a save or load operation.
         }
 
         public void b() {
-            // Probably a NOOP
-            //System.err.println("InstanceChunkLoader.a()");
-            //Logger.getLogger(CreateInstance.class.getName()).log(Level.SEVERE, null, new Exception());
+            // XXX: Can't guess if this is a save or load operation.
         }
 
-        private void initChunk(Chunk cacheChunk) {
-            List<Entity> entities = new ArrayList<Entity>();
-            List<TileEntity> tileEntities = new ArrayList<TileEntity>();
-            for (List<Entity> l : cacheChunk.entitySlices) {
-                entities.addAll(l);
-                l.clear();
-            }
-            tileEntities.addAll(cacheChunk.tileEntities.values());
-            for (Entity e : entities) {
-                cacheChunk.a(e);
-            }
-
-            for (TileEntity e : tileEntities) {
-                cacheChunk.a(e);
-            }
+        public boolean c() {
+            // Looks like a flush() / sync() method.
+            return saveLoader.c();
         }
     }
 }
