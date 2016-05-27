41f5dd9c142f4dce7d11e8218b1e47d8e2c83c3d
Antony Riley <antony@cyberiantiger.org>
2016-05-27 10:47:11 +0300

    Fix NPE in worldguard hooks when a world has no region manager.

--- instances-core/src/main/java/org/cyberiantiger/minecraft/instances/unsafe/depend/WorldGuardWorldInheritanceFactory.java
+++ instances-core/src/main/java/org/cyberiantiger/minecraft/instances/unsafe/depend/WorldGuardWorldInheritanceFactory.java
@@ -4,52 +4,15 @@
  */
 package org.cyberiantiger.minecraft.instances.unsafe.depend;
 
-import com.sk89q.worldguard.bukkit.ConfigurationManager;
 import com.sk89q.worldguard.bukkit.RegionContainer;
-import com.sk89q.worldguard.bukkit.WorldConfiguration;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.GlobalRegionManager;
 import com.sk89q.worldguard.protection.managers.RegionContainerImpl;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.util.Normal;
-import java.io.File;
 import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Level;
-import org.bukkit.BlockChangeDelegate;
-import org.bukkit.Chunk;
-import org.bukkit.ChunkSnapshot;
-import org.bukkit.Difficulty;
-import org.bukkit.Effect;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.TreeType;
-import org.bukkit.World;
-import org.bukkit.WorldBorder;
-import org.bukkit.WorldType;
-import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.FallingBlock;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.LightningStrike;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.generator.BlockPopulator;
-import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.util.Vector;
 import org.cyberiantiger.minecraft.instances.Instances;
 import org.cyberiantiger.minecraft.instances.util.DependencyFactory;
 
@@ -93,9 +56,11 @@ public class WorldGuardWorldInheritanceFactory extends DependencyFactory<Instanc
                 RegionContainer regionContainer = worldGuard.getRegionContainer();
                 RegionContainerImpl regionContainerImpl = (RegionContainerImpl) regionContainerContainer.get(regionContainer);
                 RegionManager parentRegionManager = regionContainerImpl.get(parent);
-                Normal childNormal = Normal.normal(child);
-                ConcurrentMap<Normal, RegionManager> mapping = (ConcurrentMap<Normal, RegionManager>) regionContainerImplMapping.get(regionContainerImpl);
-                mapping.putIfAbsent(childNormal, parentRegionManager);
+                if (parentRegionManager != null) {
+                    Normal childNormal = Normal.normal(child);
+                    ConcurrentMap<Normal, RegionManager> mapping = (ConcurrentMap<Normal, RegionManager>) regionContainerImplMapping.get(regionContainerImpl);
+                    mapping.putIfAbsent(childNormal, parentRegionManager);
+                }
             } catch (IllegalArgumentException | IllegalAccessException ex) {
                 getThisPlugin().getLogger().log(Level.WARNING, null, ex);
             }
